073cd416b87ce3da45c57ee23cc0d17356b66794
PhucNguyen215 <101132367+PhucNguyen215@users.noreply.github.com>
2022-03-08 16:30:13 +0700

    Add files via upload
    create card

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Chị Yến';
+const giftUrl = 'https://www.facebook.com/bui.baoyen.902';
+const eventName = 'Chúc mừng 8-3';
+const titleCard = 'Yến đáng yêu nhất';
+const contentCard = 'Chúc chị iu có ngày 8/3 tràn ngập niềm vui. Chúc chị luôn xinh đẹp, hạnh phúc, thành công, luôn gặp đc những điều may mắn. Mong những điều tốt đẹp nhất sẽ luôn đến vs chị nhaa. Luv luv💘';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
